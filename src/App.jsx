88e9d5b794da88b07292df41feef136d96ffc8f8
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-05-17 16:30:59 -0500

    refactor: update App component structure and styling for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import '../global.css'
-import Experience from './components/Experience';
-import Header from './components/Header'
-import Projects from './components/Projects'
+import "../global.css";
+import Experience from "./components/Experience";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Tech from "./components/Tech";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <section className='w-screen h-screen p-0 m-0 overflow-x-hidden font-Geist'>
-      <Header/>
-      <main className='w-full h-auto mt-6 mx-3 pb-12'>
-        <Projects/>
+    <div className="w-full min-h-screen p-0 m-0 overflow-x-hidden font-Geist flex flex-col bg-white">
+      <Header />
+      <main className="w-full max-w-7xl mx-auto flex flex-col gap-20 pt-28 pb-20 px-4">
+        <Projects />
+        <Experience />
+        <Tech/>
       </main>
-    </section>
-  )
+      <Footer/>
+    </div>
+  );
 }
 
-export default App
+export default App;
